f77fd1aa811ebcbfc6476564ce304bcf5cdb77c8
Jon-Felipe <rodriguesjf7@gmail.com>
2025-06-02 16:53:40 +0200

    first form action

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,11 @@
 export default function Signup() {
+  function signupAction(formData) {
+    const email = formData.get('email');
+    console.log(email);
+  }
+
   return (
-    <form>
+    <form action={signupAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
